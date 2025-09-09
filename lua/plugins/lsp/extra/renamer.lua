2c19f32140a4d59b8f9ae200158b3aa54bdcd66f
AlexRosas <rosasrias82@gmail.com>
2025-09-09 16:06:10 -0500

    fix(renamer): Fix config to renamer words

--- lua/plugins/lsp/extra/renamer.lua
+++ lua/plugins/lsp/extra/renamer.lua
@@ -1,95 +1,97 @@
-local Input = require("nui.input")
-local event = require("nui.utils.autocmd").event
-local NuiText = require("nui.text")
+local lsp = vim.lsp
+local api = vim.api
 
-local function nui_lsp_rename()
-	local current_name = vim.fn.expand("<cword>")
-	local params = vim.lsp.util.make_position_params(0, vim.bo.fileencoding)
-
-	local function on_submit(new_name)
-		if not new_name or #new_name == 0 then
-			vim.api.nvim_notify("Cancelled: New name is empty!", vim.log.levels.INFO, {
-				icon = "ﰸ",
-				title = " LSP",
-			})
-			return
-		elseif new_name == current_name then
-			vim.api.nvim_notify("Cancelled: New and current names are the same!", vim.log.levels.INFO, {
-				icon = "",
-				title = " LSP",
-			})
-			return
-		end
-
-		params.newName = new_name
-		vim.lsp.buf_request(0, "textDocument/rename", params, function(_, result, context, _)
-			if not result then
-				return
-			end
-			local client = vim.lsp.get_client_by_id(context.client_id)
-			vim.lsp.util.apply_workspace_edit(result, client.offset_encoding)
-
-			local total_files = vim.tbl_count(vim.F.if_nil(result.changes, {}))
-			print(
-				string.format("Changed %s file%s. To save them run ':wa'", total_files, total_files > 1 and "s" or "")
-			)
-			vim.api.nvim_notify("Renamed " .. current_name .. " into " .. new_name .. ".", vim.log.levels.INFO, {
-				icon = "",
-				title = " LSP",
-			})
-		end)
-	end
-
-	local popup_options = {
-		border = {
-			style = {
-				top_left = NuiText(" ", "NUINormal"),
-				top = NuiText("━", "NUILine"),
-				top_right = NuiText(" ", "NUINormal"),
-				left = NuiText(" ", "NUINormal"),
-				right = NuiText(" ", "NUINormal"),
-				bottom_left = NuiText(" ", "NUINormal"),
-				bottom = NuiText(" ", "NUINormal"),
-				bottom_right = NuiText(" ", "NUINormal"),
-			},
-			text = {
-				top = NuiText("RENAME ", "NUIHeading"),
-				top_align = "left",
-			},
-		},
-		winblend = 0,
-		highlight = "NUIText:NUIText",
-		relative = {
-			type = "buf",
-			position = {
-				row = params.position.line,
-				col = params.position.character,
-			},
-		},
-		position = {
-			row = 1,
-			col = 0,
-		},
-		size = {
-			width = 25,
-			height = 1,
-		},
-	}
-
-	local input = Input(popup_options, {
-		default_value = current_name,
-		on_submit = on_submit,
-		prompt = NuiText(" ﬦ ", "NUIPrompt"),
-	})
-
-	input:mount()
-	vim.schedule(function()
-		vim.api.nvim_command("stopinsert")
-	end)
-	input:map("n", "<esc>", input.input_props.on_close, { noremap = true })
-	input:on(event.BufLeave, input.input_props.on_close, { once = true })
+local function get_text_at_range(range, position_encoding)
+	return api.nvim_buf_get_text(
+		0,
+		range.start.line,
+		lsp.util._get_line_byte_from_position(0, range.start, position_encoding),
+		range["end"].line,
+		lsp.util._get_line_byte_from_position(0, range["end"], position_encoding),
+		{}
+	)[1]
 end
 
-return {
-	lsp_rename = nui_lsp_rename,
-}
+local function get_symbol_to_rename(cb)
+	local cword = vim.fn.expand("<cword>")
+	local clients = lsp.get_clients({ bufnr = 0, method = "textDocument/rename" })
+
+	if #clients == 0 then
+		cb(cword)
+		return
+	end
+
+	-- Prefer clients that support prepareRename
+	table.sort(clients, function(a, b)
+		return a:supports_method("textDocument/prepareRename") and not b:supports_method("textDocument/prepareRename")
+	end)
+
+	local client = clients[1]
+
+	if client:supports_method("textDocument/prepareRename") then
+		local params = lsp.util.make_position_params(0, client.offset_encoding)
+
+		client:request("textDocument/prepareRename", params, function(err, result, _, _)
+			if err or not result then
+				cb(cword)
+			end
+
+			local symbol_text = cword
+
+			if result.placeholder then
+				symbol_text = result.placeholder
+			elseif result.start then
+				symbol_text = get_text_at_range(result, client.offset_encoding)
+			elseif result.range then
+				symbol_text = get_text_at_range(result.range, client.offset_encoding)
+			end
+
+			cb(symbol_text)
+		end, 0)
+	else
+		cb(cword)
+	end
+end
+
+return function()
+	get_symbol_to_rename(function(to_rename)
+		local buf = api.nvim_create_buf(false, true)
+
+		local winopts = {
+			height = 1,
+			style = "minimal",
+			border = "single",
+			row = 1,
+			col = 1,
+			relative = "cursor",
+			width = #to_rename + 15,
+			title = { { " Renamer ", "@comment.danger" } },
+			title_pos = "center",
+		}
+
+		local win = api.nvim_open_win(buf, true, winopts)
+		vim.wo[win].winhl = "Normal:Normal,FloatBorder:Removed"
+		api.nvim_set_current_win(win)
+
+		api.nvim_buf_set_lines(buf, 0, -1, true, { " " .. to_rename })
+
+		vim.bo[buf].buftype = "prompt"
+		vim.fn.prompt_setprompt(buf, "")
+		vim.api.nvim_input("A")
+
+		vim.keymap.set({ "i", "n" }, "<Esc>", function()
+			api.nvim_buf_delete(buf, { force = true })
+		end, { buffer = buf })
+
+		vim.fn.prompt_setcallback(buf, function(text)
+			local newName = vim.trim(text)
+			api.nvim_buf_delete(buf, { force = true })
+
+			if #newName > 0 and newName ~= to_rename then
+				local params = lsp.util.make_position_params(0, "utf-8")
+				params = vim.tbl_extend("force", params, { newName = newName })
+				lsp.buf_request(0, "textDocument/rename", params)
+			end
+		end)
+	end)
+end
