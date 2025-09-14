c961b97e8c67eea58dfbe70ca5dbc778bf402686
AlexRosas <rosasrias82@gmail.com>
2025-09-13 21:56:56 -0500

    feat(bufferline): upgrade config

--- lua/plugins/ui/bufferline.lua
+++ lua/plugins/ui/bufferline.lua
@@ -1,112 +1,129 @@
 return {
-	"akinsho/bufferline.nvim",
-	event = { "BufReadPost", "BufNewFile", "BufAdd" },
-	dependencies = {
-		"nvim-tree/nvim-web-devicons",
-		"echasnovski/mini.bufremove",
-	},
-	opts = {
-		options = {
-			mode = "buffers",
-			numbers = "none",
-			offsets = {
-				{
-					filetype = "NvimTree",
-				},
-			},
-			buffer_close_icon = "",
-			close_command = "bdelete! %d",
-			right_mouse_command = "bdelete! %d",
-			left_mouse_command = "buffer %d",
-			middle_mouse_command = "bdelete! %d",
-			modified_icon = "",
-			left_trunc_marker = "",
-			right_trunc_marker = "",
-			max_name_length = 14,
-			max_prefix_length = 15,
-			truncate_names = true,
-			tab_size = 15,
-			always_show_bufferline = true,
-			show_buffer_icons = true,
-			show_buffer_close_icons = true,
-			show_tab_indicators = true,
-			show_duplicate_prefix = true,
-			persist_buffer_sort = true,
-			show_close_icon = true,
-			color_icons = true,
-			enforce_regular_tabs = true,
-			separator_style = { "any", "any" }, -- slant, slope, thick, thin, {'any', 'any'}
-			indicator = { style = "none" },
-			exclude_ft = { "help", "alpha", "toggleterm", "lir", "dashboard" }, -- Corregido posible typo en 'dashboard'
-			custom_filter = function(bufnr)
-				return vim.bo[bufnr].buflisted
-			end,
-			themable = true,
-			custom_areas = {
-				right = function()
-					return {
-						{
-							text = "%@v:lua.RunBuffer@%#BufferlineRun#  %X",
-							highlight = "BufferlineRun",
-						},
-						{
-							text = "%@v:lua.SplitBuffer@%#BufferlineSplit#  %X",
-							highlight = "BufferlineSplit",
-						},
-						{
-							text = "%@v:lua.ToggleTransparency@%#BufferlineTrasparency# 󱡓 %X",
-							highlight = "BufferlineTrasparency",
-						},
-						{
-							text = "%@v:lua.ToggleTheme@%#BufferlineTheme# 󱥚 %X",
-							highlight = "BufferlineTheme",
-						},
-						{
-							text = "%@v:lua.CloseAll@%#BufferlineCloseAll# 󱎘 %X",
-							highlight = "BufferlineCloseAll",
-						},
-					}
-				end,
-			},
-		},
-	},
-	config = function(_, opts)
-		function _G.SplitBuffer()
-			vim.cmd("vsplit")
-		end
+  "akinsho/bufferline.nvim",
+  event = { "BufReadPost", "BufNewFile", "BufAdd" },
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+    "echasnovski/mini.bufremove",
+  },
+  opts = {
+    options = {
+      mode = "buffers",
+      numbers = "none",
+      offsets = {
+        {
+          filetype = "NvimTree",
+          text = "File Explorer",
+          highlight = "Directory",
+          separator = true,
+        },
+      },
+      buffer_close_icon = "",
+      close_command = "bdelete! %d",
+      right_mouse_command = "bdelete! %d",
+      left_mouse_command = "buffer %d",
+      middle_mouse_command = "bdelete! %d",
+      modified_icon = " ",
+      left_trunc_marker = "|",
+      right_trunc_marker = "|",
+      max_name_length = 14,
+      max_prefix_length = 15,
+      truncate_names = true,
+      tab_size = 15,
+      always_show_bufferline = true,
+      show_buffer_icons = true,
+      show_buffer_close_icons = true,
+      show_tab_indicators = true,
+      show_duplicate_prefix = true,
+      persist_buffer_sort = true,
+      show_close_icon = true,
+      color_icons = true,
+      enforce_regular_tabs = true,
+      separator_style = { "any", "any" }, -- slant, slope, thick, thin, {'any', 'any'}
+      indicator = { style = "none" },
+      exclude_ft = { "help", "alpha", "toggleterm", "lir", "dashboard" },
+      custom_filter = function(bufnr)
+        return vim.bo[bufnr].buflisted
+      end,
+      themable = true,
+      custom_areas = {
+        right = function()
+          return {
+            {
+              text = "%@v:lua.RunBuffer@%#BufferlineRun#  %X",
+              highlight = "BufferlineRun",
+            },
+            {
+              text = "%@v:lua.SplitBuffer@%#BufferlineSplit#  %X",
+              highlight = "BufferlineSplit",
+            },
+            {
+              text = "%@v:lua.ToggleTransparency@%#BufferlineTransparency# 󱡔 %X",
+              highlight = "BufferlineTransparency",
+            },
+            {
+              text = "%@v:lua.ToggleTheme@%#BufferlineTheme# 󱥚 %X",
+              highlight = "BufferlineTheme",
+            },
+            {
+              text = "%@v:lua.CloseAll@%#BufferlineCloseAll# 󰅗 %X",
+              highlight = "BufferlineCloseAll",
+            },
+          }
+        end,
+      },
+    },
+    highlights = require "themes.integrations.bufferline",
+  },
+  config = function(_, opts)
+    function _G.SplitBuffer()
+      vim.cmd "vsplit"
+    end
 
-		function _G.RunBuffer()
-			require("core.functions").build_run()
-		end
+    function _G.RunBuffer()
+      require("core.functions").build_run()
+    end
 
-		function _G.ToggleTheme()
-			require("themes.pick").random()
-		end
+    function _G.ToggleTheme()
+      require("themes.pick").random()
+    end
 
-		function _G.ToggleTransparency()
-			require("themes").toggle_transparency()
-		end
+    function _G.ToggleTransparency()
+      require("themes").toggle_transparency()
+    end
 
-		function _G.CloseAll()
-			local bd = require("mini.bufremove").delete
-			for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
-				if vim.bo[bufnr].buflisted then
-					bd(bufnr, true)
-				end
-			end
-		end
+    function _G.CloseAll()
+      local bd = require("mini.bufremove").delete
+      for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
+        if vim.bo[bufnr].buflisted then
+          bd(bufnr, true)
+        end
+      end
+    end
 
-		require("bufferline").setup(opts)
+    -- Inicializar bufferline
+    ---@diagnostic disable-next-line: different-requires
+    require("bufferline").setup(opts)
 
-		local map = vim.keymap.set
-		local bd = require("mini.bufremove").delete
+    -- 👇 Definir highlights custom (que no son de Bufferline oficial)
+    local colors = require("themes").get_theme_tb "base_30"
+    local blend = require("themes.colors").blend
 
-		map("n", "<Tab>", "<Cmd>BufferLineCycleNext<CR>", { desc = "Next buffer" })
-		map("n", "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", { desc = "Previous buffer" })
-		map("n", "<C-q>", function()
-			bd(0, true)
-		end, { desc = "Close buffer" })
-		map("n", "<leader>bc", "<Cmd>BufferLinePickClose<CR>", { desc = "Pick buffer to close" })
-		map("n", "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", { desc = "Toggle pin buffer" })
-	end,
+    vim.api.nvim_set_hl(0, "BufferlineRun", { fg = colors.teal, bg = blend(colors.teal, colors.black, 0.15) })
+    vim.api.nvim_set_hl(0, "BufferlineSplit", { fg = colors.green, bg = blend(colors.green, colors.black, 0.15) })
+    vim.api.nvim_set_hl(0, "BufferlineTheme", { fg = colors.yellow, bg = blend(colors.yellow, colors.black, 0.15) })
+    vim.api.nvim_set_hl(0, "BufferlineTransparency", { fg = colors.blue, bg = blend(colors.blue, colors.black, 0.15) })
+    vim.api.nvim_set_hl(0, "BufferlineCloseAll", { fg = colors.red, bg = blend(colors.red, colors.black, 0.15) })
+
+    -- Keymaps
+    local map = vim.keymap.set
+    local bd = require("mini.bufremove").delete
+
+    map("n", "<Tab>", "<Cmd>BufferLineCycleNext<CR>", { desc = "Next buffer" })
+    map("n", "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", { desc = "Previous buffer" })
+    map("n", "<C-q>", function()
+      bd(0, true)
+    end, { desc = "Close buffer" })
+    map("n", "<leader>bc", "<Cmd>BufferLinePickClose<CR>", { desc = "Pick buffer to close" })
+    map("n", "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", { desc = "Toggle pin buffer" })
+  end,
 }
--- lua/themes/integrations/bufferline.lua
+++ lua/themes/integrations/bufferline.lua
@@ -1,70 +1,22 @@
 local colors = require("themes").get_theme_tb "base_30"
-local blend = require("themes.colors").blend
-local devicons = require "nvim-web-devicons"
 
-local function sanitize(name)
-  name = tostring(name)
-  return name:gsub("[^%w_]", "_")
-end
+return {
+  fill = { bg = colors.black },
+  background = { fg = colors.grey, bg = colors.black },
 
-local function generate_icon_colors()
-  local icons = {}
+  buffer_visible = { fg = colors.grey_fg, bg = colors.black },
+  buffer_selected = { fg = colors.blue, bg = colors.one_bg, bold = true },
 
-  for _, icon_data in pairs(devicons.get_icons()) do
-    local name = sanitize(icon_data.name or "Default")
-    local hl_name = "BufferLineDevIcon" .. name
+  separator = { fg = colors.black, bg = colors.black },
+  separator_selected = { fg = colors.one_bg, bg = colors.one_bg },
+  separator_visible = { fg = colors.black, bg = colors.black },
 
-    -- Normal: color original del icono, fondo negro
-    icons[hl_name] = {
-      fg = icon_data.color or colors.white,
-      bg = colors.black,
-    }
+  indicator_selected = { fg = colors.blue, bg = colors.one_bg },
+  modified = { fg = colors.green, bg = colors.black },
+  modified_visible = { fg = colors.green, bg = colors.black },
+  modified_selected = { fg = colors.green, bg = colors.one_bg },
 
-    -- Selected: todos uniformes
-    icons[hl_name .. "Selected"] = {
-      fg = colors.black,
-      bg = colors.blue,
-    }
-  end
-
-  return icons
-end
-
-local icon_styles = generate_icon_colors()
-
-return vim.tbl_extend("force", {
-  BufferlineRun = { fg = colors.teal, bg = blend(colors.teal, colors.black, 0.15) },
-  BufferlineSplit = { fg = colors.green, bg = blend(colors.green, colors.black, 0.15) },
-  BufferlineTheme = { fg = colors.yellow, bg = blend(colors.yellow, colors.black, 0.15) },
-  BUfferlineTrasparency = { fg = colors.blue, bg = blend(colors.blue, colors.black, 0.15) },
-  BufferlineCloseAll = { fg = colors.red, bg = blend(colors.red, colors.black, 0.15) },
-
-  BufferLineBackground = { fg = colors.light_grey, bg = colors.black },
-  BufferLineBuffer = { fg = colors.grey, bg = colors.black },
-  BufferLineBufferSelected = { fg = colors.black, bg = colors.blue, bold = true },
-  BufferLineBufferVisible = { fg = colors.blue, bg = colors.black },
-  BufferLineCloseButton = { fg = colors.red, bg = colors.black },
-  BufferLineCloseButtonSelected = { fg = colors.black, bg = colors.blue },
-  BufferLineCloseButtonVisible = { fg = colors.red, bg = colors.black },
-  BufferLineDuplicate = { fg = colors.dark_purple, bg = colors.black },
-  BufferLineDuplicateSelected = { fg = colors.dark_purple, bg = colors.black },
-  BufferLineDuplicateVisible = { fg = colors.dark_purple, bg = colors.black },
-  BufferLineFill = { fg = colors.grey, bg = colors.black },
-  BufferLineIndicatorSelected = { fg = colors.blue, bg = colors.blue },
-  BufferLineIndicatorVisible = { fg = colors.black, bg = colors.black },
-  BufferLineModified = { fg = colors.green, bg = colors.black },
-  BufferLineModifiedSelected = { fg = colors.black, bg = colors.blue },
-  BufferLineModifiedVisible = { fg = colors.green, bg = colors.black },
-  BufferLineOffsetSeparator = { fg = colors.blue, bg = colors.blue },
-  BufferLinePick = { fg = colors.sun, bg = colors.black, bold = true },
-  BufferLinePickSelected = { fg = colors.light_grey, bg = colors.blue, bold = true },
-  BufferLinePickVisible = { fg = colors.light_grey, bg = colors.black, bold = true },
-  BufferLineSeparator = { fg = colors.black, bg = colors.black },
-  BufferLineSeparatorSelected = { fg = colors.blue, bg = colors.blue },
-  BufferLineSeparatorVisible = { fg = colors.black, bg = colors.black },
-  BufferLineTabClose = { fg = colors.red, bg = colors.black },
-  BufferLineTab = { fg = colors.grey, bg = colors.black },
-  BufferLineTabSelected = { fg = colors.blue, bg = colors.black, bold = true },
-  BufferLineTabSeparator = { fg = colors.black, bg = colors.black },
-  BufferLineTabSeparatorSelected = { fg = colors.black, bg = colors.black },
-}, icon_styles)
+  close_button = { fg = colors.light_grey, bg = colors.black },
+  close_button_visible = { fg = colors.light_grey, bg = colors.black },
+  close_button_selected = { fg = colors.red, bg = colors.one_bg },
+}
