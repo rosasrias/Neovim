e0dba7a70b5376b15848ab7db9e427a764b576dc
AlexRosas <rosasrias82@gmail.com>
2026-01-08 22:23:27 -0500

    formating

--- lua/plugins/lsp/extra/format.lua
+++ lua/plugins/lsp/extra/format.lua
@@ -1,27 +1,27 @@
 local M = {}
 local api = vim.api
 local cmp_ui = {
-	format_colors = { lsp = true, icon = "󱓻" },
+  format_colors = { lsp = true, icon = "󱓻" },
 }
 local icon = cmp_ui.format_colors.icon .. " "
 
 local hlcache = {}
 
 M.lsp = function(entry, item, kind_txt)
-	local color = entry.completion_item.documentation
+  local color = entry.completion_item.documentation
 
-	if color and type(color) == "string" and color:match("^#%x%x%x%x%x%x$") then
-		local hl = "hex-" .. color:sub(2)
+  if color and type(color) == "string" and color:match "^#%x%x%x%x%x%x$" then
+    local hl = "hex-" .. color:sub(2)
 
-		if not hlcache[hl] then
-			api.nvim_set_hl(0, hl, { fg = color })
-			hlcache[hl] = true
-		end
+    if not hlcache[hl] then
+      api.nvim_set_hl(0, hl, { fg = color })
+      hlcache[hl] = true
+    end
 
-		item.kind = (icon or (" " .. icon)) .. kind_txt
-		item.kind_hl_group = hl
-		item.menu_hl_group = hl
-	end
+    item.kind = (icon or (" " .. icon)) .. kind_txt
+    item.kind_hl_group = hl
+    item.menu_hl_group = hl
+  end
 end
 
 return M
