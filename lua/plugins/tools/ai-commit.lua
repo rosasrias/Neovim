721db01ed7d46e749dd8d5e08c13dafc6bce23be
Alex Rosas <rosasrias82@gmail.com>
2026-06-05 22:17:18 -0500

    fix(AICommit): fix errors

--- lua/plugins/tools/ai-commit.lua
+++ lua/plugins/tools/ai-commit.lua
@@ -6,7 +6,7 @@ return {
   },
   config = function()
     require("ai-commit").setup {
-      openrouter_api_key = "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",
+      openrouter_api_key = "",
       model = "poolside/laguna-m.1:free",
       auto_push = false,
     }
